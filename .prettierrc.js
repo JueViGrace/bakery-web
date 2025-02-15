1b3d21b47636f3d939c0669bd5df8e162d6982ef
Daniel Zabala <danielzabalanava@gmail.com>
2025-02-15 09:17:35 -0400

    removed prettier-plugin-tailwind

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,7 +2,7 @@ export default {
   trailingComma: 'es5',
   semi: true,
   singleQuote: true,
-  plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwind'],
+  plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
   overrides: [
     {
       files: '*.astro',
